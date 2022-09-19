cba36261caf1340e9f709b781518b6cd8d4c1837
Hector Martinez <hekthor.martinez@gmail.com>
2022-09-19 09:35:17 +0200

    fps write video change to 20

--- process-servers/deepLearning/testOD.py
+++ process-servers/deepLearning/testOD.py
@@ -91,7 +91,7 @@ if __name__ == "__main__":
         frame_width = int(cap.get(3))
         frame_height = int(cap.get(4))
         # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.          
-        out = cv2.VideoWriter(outFile, cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+        out = cv2.VideoWriter(outFile, cv2.VideoWriter_fourcc('M','J','P','G'), 20, (frame_width,frame_height))
 
         while(cap.isOpened()):
             ret, img = cap.read()
